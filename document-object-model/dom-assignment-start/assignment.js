d64c721be067c4261f4a5cd74b3e01a84e046e5f
Silver <silverdev@protonmail.com>
2023-08-24 13:52:50 +0300

    DOM finish task 2

--- document-object-model/dom-assignment-start/assignment.js
+++ document-object-model/dom-assignment-start/assignment.js
@@ -3,3 +3,8 @@ const elementSelection2 = document.querySelector("li");
 
 elementSelection.style.color = "white";
 elementSelection2.style.backgroundColor = "black";
+
+const elementTitleSelection = document.querySelector("title");
+// elementTitleSelection.textContent = "Assignment - Solved!";
+const elementTitleSelection2 = document.head.querySelector("title");
+elementTitleSelection2.textContent = "Assignment - Solved!";
