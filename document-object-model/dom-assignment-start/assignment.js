f4facbc5019b6840c1313e1c6887c29a8282b619
Silver <silverdev@protonmail.com>
2023-08-24 13:40:37 +0300

    DOM finish task 1

---
+++ document-object-model/dom-assignment-start/assignment.js
@@ -0,0 +1,5 @@
+const elementSelection = document.getElementById("task-1");
+const elementSelection2 = document.querySelector("li");
+
+elementSelection.style.color = "white";
+elementSelection2.style.backgroundColor = "black";
