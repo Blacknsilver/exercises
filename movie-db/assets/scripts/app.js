9231c0fa6fa1fd4b685ac780c86ae351db7ed12d
Silver <silverdev@protonmail.com>
2023-08-25 10:17:10 +0300

    Add a couple button selectors.

---
+++ movie-db/assets/scripts/app.js
@@ -0,0 +1,5 @@
+const addMovieModal = document.getElementById("add-modal");
+// const addMovieModal = document.body.children[1];
+
+const startAddMovieButton = document.querySelector("header button");
+// querySelector.lastElementChild; would also work
