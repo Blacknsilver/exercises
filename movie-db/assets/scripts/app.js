cabfcbc0cb6ec22d3b0b67294ed8ddba3887f6b4
Silver <silverdev@protonmail.com>
2023-08-25 10:31:32 +0300

    Add functionality to the "add movie" button (turn visible/invisible).

--- movie-db/assets/scripts/app.js
+++ movie-db/assets/scripts/app.js
@@ -1,5 +1,13 @@
 const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.body.children[1];
+//const addMovieModal = document.querySelector('add-modal');
 
 const startAddMovieButton = document.querySelector("header button");
 // querySelector.lastElementChild; would also work
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+};
+//open Modal if it's closed and vice-versa
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
