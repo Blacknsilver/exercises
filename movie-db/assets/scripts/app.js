550e20bdfd5f6ded848cc58d7d2dc53919cdc046
Silver <silverdev@protonmail.com>
2023-08-25 13:45:12 +0300

    Add functionality for toggling background visibility whenever the user clicks the button.

--- movie-db/assets/scripts/app.js
+++ movie-db/assets/scripts/app.js
@@ -1,12 +1,20 @@
 const addMovieModal = document.getElementById("add-modal");
 // const addMovieModal = document.body.children[1];
-//const addMovieModal = document.querySelector('add-modal');
+// const addMovieModal = document.querySelector('add-modal');
 
 const startAddMovieButton = document.querySelector("header button");
 // querySelector.lastElementChild; would also work
 
+const backdrop = document.getElementById("backdrop");
+// const backdrop = document.body.firstElementChild;
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
 };
 //open Modal if it's closed and vice-versa
 
