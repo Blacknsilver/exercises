6fab07150a2ffdc19614e60fa9302caa315fb104
Silver <silverdev@protonmail.com>
2023-08-28 08:20:59 +0300

    Add functionality for deleting movie entries when the user clicks on them.

--- movie-db/assets/scripts/app.js
+++ movie-db/assets/scripts/app.js
@@ -20,7 +20,20 @@ const updateUI = () => {
   }
 }; // If there are movies, hide the "your movie database" message up top
 
-const renderNewMovieElement = (title, imageUrl, rating) => {
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex, 1); // Items will move forward by 1 space after the target is deleted
+  const listRoot = document.getElementById("movie-list");
+  listRoot.children[movieIndex].remove();
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
   const newMovieElement = document.createElement("li");
   newMovieElement.className = "movie-element";
   newMovieElement.innerHTML = `
@@ -32,6 +45,7 @@ const renderNewMovieElement = (title, imageUrl, rating) => {
     <p>${rating}/5 stars</p>
     </div>
    `;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
   const listRoot = document.getElementById("movie-list");
   listRoot.append(newMovieElement);
 };
@@ -75,6 +89,7 @@ const addMovieHandler = () => {
     return;
   } // Validates whether the user inputs are valid.
   const newMovie = {
+    id: Math.random().toString(),
     title: titleValue,
     image: imageUrlValue,
     rating: ratingValue,
@@ -82,7 +97,12 @@ const addMovieHandler = () => {
   movies.push(newMovie); // Adds a new movie to the movies array.
   toggleMovieModal();
   clearMovieInputs();
-  renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
   updateUI();
 };
 
