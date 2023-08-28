c2b37e3dd27557f0c33b40890d54148e60611573
Silver <silverdev@protonmail.com>
2023-08-28 15:02:47 +0300

    Add slice() exercises

--- arrays/app.js
+++ arrays/app.js
@@ -15,19 +15,33 @@
 // const numbers4 = Array.from(listItems); // Turns an array-like object, such as a string or a node list, into an actual array
 // console.log(numbers4);
 
-const hobbies = ["Sports", "Cooking"];
-hobbies.push("Reading"); // Adds a new item to the end of the array
-hobbies.unshift("Running"); // Adds an element to the start of the array
+// const hobbies = ["Sports", "Cooking"];
+// hobbies.push("Reading"); // Adds a new item to the end of the array
+// hobbies.unshift("Running"); // Adds an element to the start of the array
 
-const popped = hobbies.pop(); // Removes last element and optionally puts it into a new var
-hobbies.shift(); // Removes item from the beginning of an array. Shifts all elements to the left
+// const popped = hobbies.pop(); // Removes last element and optionally puts it into a new var
+// hobbies.shift(); // Removes item from the beginning of an array. Shifts all elements to the left
 
-hobbies[1] = "Dancing"; // Set existing element to something else
+// hobbies[1] = "Dancing"; // Set existing element to something else
 
-hobbies.splice(1, 0, "Eating"); // Not useable on array-like objects
+// hobbies.splice(1, 0, "Eating"); // Not useable on array-like objects
 
-console.log(hobbies);
+// console.log(hobbies);
 
-hobbies.splice(0, 1, "Drinking");
+// hobbies.splice(0, 1, "Drinking");
 
-console.log(hobbies);
+// console.log(hobbies);
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+
+console.log(testResults.slice());
+const storedResults = testResults.slice(); // Returns a brand new array
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
+
+storedResults2 = testResults.slice(0, 2); // Finds and adds the elements from the first index (included) to the second (not included). Can also use negative numbers but
+// both arguments have to be negative.
+console.log(storedResults2);
+
+console.log(testResults.slice(2)); //Using a single argument will output everything from that element (included) to the end.
