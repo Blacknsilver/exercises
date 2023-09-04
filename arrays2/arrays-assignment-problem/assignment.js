78abfb8bc319b5a2a5b71d641c8600e883dcd6ea
Silver <silverdev@protonmail.com>
2023-09-04 15:15:55 +0300

    Solve first array task.

---
+++ arrays2/arrays-assignment-problem/assignment.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const filteredNumbers = numbers.filter((val) => val > 5);
+console.log(filteredNumbers);
+
+const mappedNumbers = numbers.map((val) => ({ num: val }));
+console.log(mappedNumbers);
+
+const multiplication = numbers.reduce((currResult, currVal) => {
+  return currResult * currVal;
+}, 1); // Have to start with 1, starting multiplication with 0 results in nothing but 0s.
+console.log(multiplication);
