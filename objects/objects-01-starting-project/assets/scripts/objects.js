8e0e5c7609b9b6b574a3eb5b4684954b5e5ba7f0
Silver <silverdev@protonmail.com>
2023-09-06 09:50:18 +0300

    Add special key name exercise.

--- objects/objects-01-starting-project/assets/scripts/objects.js
+++ objects/objects-01-starting-project/assets/scripts/objects.js
@@ -1,13 +1,24 @@
+// const person = {
+//   name: "Max",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   greet: function () {
+//     alert("Hello There");
+//   },
+// };
+// person.isAdmin = true; // This works just fine even though person is a const.
+// person.age = 31; // This also works.
+// delete person.name; // This is how to remove a property from an object.
+// // person.age = undefined // This works but is not as clean. Null also works but delete is optimal.
+// console.log(person);
+
 const person = {
-  name: "Max",
+  "first name": "Max", // this is how strings can be used as a key in objects
   age: 30,
   hobbies: ["Sports", "Cooking"],
   greet: function () {
     alert("Hello There");
   },
 };
-person.isAdmin = true; // This works just fine even though person is a const.
-person.age = 31; // This also works.
-delete person.name; // This is how to remove a property from an object.
-// person.age = undefined // This works but is not as clean. Null also works but delete is optimal.
-console.log(person);
+
+console.log(person["first name"]); // This is how to access string keys.
