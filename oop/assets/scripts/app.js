118dc72ad71037694081de65e5a18763e39105f1
Silver <silverdev@protonmail.com>
2023-09-11 20:53:52 +0300

    Add a productList with a couple of basic products.

---
+++ oop/assets/scripts/app.js
@@ -0,0 +1,18 @@
+const productList = {
+  products: [
+    {
+      title: "A Pillow",
+      imageUrl:
+        "https://m.media-amazon.com/images/I/51HGSKCft0L._AC_UL320_.jpg",
+      price: 19.99,
+      description: "A soft pillow",
+    },
+    {
+      title: "A Carpet",
+      imageUrl:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/220px-Wollteppich_1.jpg",
+      price: 89.99,
+      description: "A carpet",
+    },
+  ],
+};
