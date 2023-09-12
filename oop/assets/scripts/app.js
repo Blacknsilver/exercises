6066bda339a7a477045619747852223ad85c857f
Silver <silverdev@protonmail.com>
2023-09-12 23:02:23 +0300

    Add logic for updating the price total.

--- oop/assets/scripts/app.js
+++ oop/assets/scripts/app.js
@@ -13,6 +13,26 @@ class Product {
 
 class ShoppingCart {
   items = [];
+
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  addProduct(product) {
+    const updatedItems = [...this.items];
+    updatedItems.push(product);
+    this.cartItems = updatedItems;
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -20,6 +40,7 @@ class ShoppingCart {
     <button>Order now.</button>
     `;
     cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
   }
 }
@@ -29,8 +50,7 @@ class ProductItem {
     this.product = product;
   }
   addToCart() {
-    console.log("Adding product to cart");
-    console.log(this.product);
+    App.addProductToCart(this.product);
   }
 
   render() {
@@ -85,14 +105,23 @@ class Shop {
   render() {
     const renderHook = document.getElementById("app");
 
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
     const productList = new ProductList();
     const prodListEl = productList.render();
     renderHook.append(cartEl);
     renderHook.append(prodListEl);
   }
 }
-
-const shop = new Shop();
-shop.render();
+class App {
+  static cart;
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+App.init();
