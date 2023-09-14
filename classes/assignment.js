38c9be4a2c845700f10acd8113a304de97f8f767
Silver <silverdev@protonmail.com>
2023-09-14 13:34:32 +0300

    Solve second class task

--- classes/assignment.js
+++ classes/assignment.js
@@ -4,9 +4,23 @@ class Course {
     this.length = length;
     this.price = price;
   }
+  calculateValue() {
+    return this.length / this.price;
+  }
+  printSummary() {
+    console.log(
+      `Title: ${this.title}, Length: ${this.length}, Price: ${this.price}`
+    );
+  }
 }
 
 const courseOne = new Course("RandomTitle", 10, 20);
 const courseTwo = new Course("RandomOtherTitle", 15, 30);
 console.log(courseOne);
 console.log(courseTwo);
+
+console.log(courseOne.calculateValue());
+console.log(courseTwo.calculateValue());
+
+courseOne.printSummary();
+courseTwo.printSummary();
