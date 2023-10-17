0fe06e43e35990b8d8ad7a2204e03b033fc17e42
Silver <silverdev@protonmail.com>
2023-10-17 17:52:26 +0300

    Parse the response data to a javascript object.

--- http-requests/assets/scripts/app.js
+++ http-requests/assets/scripts/app.js
@@ -2,4 +2,13 @@ const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
 
+xhr.responseType = "json"; // automatically parses to JS; same as using JSON.parse
+
+xhr.onload = function () {
+  //   console.log(xhr.response);
+  //   const listOfPosts = JSON.parse(xhr.response);
+  const listOfPosts = xhr.response;
+  console.log(xhr.response);
+};
+
 xhr.send();
