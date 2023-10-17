818c4196e08362543200ffc2b54fed49eaa5573a
Silver <silverdev@protonmail.com>
2023-10-17 17:56:51 +0300

    Add code for rendering the dummy posts received  from the dummy website.

--- http-requests/assets/scripts/app.js
+++ http-requests/assets/scripts/app.js
@@ -1,3 +1,6 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
@@ -8,7 +11,13 @@ xhr.onload = function () {
   //   console.log(xhr.response);
   //   const listOfPosts = JSON.parse(xhr.response);
   const listOfPosts = xhr.response;
-  console.log(xhr.response);
+
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title.toUpperCase();
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
 };
 
 xhr.send();
