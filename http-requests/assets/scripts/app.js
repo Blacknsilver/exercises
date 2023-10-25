c4da0d9b6f1938245ac9fe39c6defd8d720803c9
Silver <silverdev@protonmail.com>
2023-10-25 12:54:49 +0300

    Add error-handling code.

--- http-requests/assets/scripts/app.js
+++ http-requests/assets/scripts/app.js
@@ -13,8 +13,16 @@ function sendHttpRequest(method, url, data) {
     xhr.responseType = "json"; // automatically parses to JS; same as using JSON.parse
 
     xhr.onload = function () {
-      resolve(xhr.response);
-      //   const listOfPosts = JSON.parse(xhr.response);
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+        //   const listOfPosts = JSON.parse(xhr.response);
+      } else {
+        reject(new Error("Something went wrong!"));
+      }
+    };
+
+    xhr.onerror = function () {
+      reject(new Error("Failed to send request!"));
     };
 
     xhr.send(JSON.stringify(data));
@@ -23,18 +31,22 @@ function sendHttpRequest(method, url, data) {
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts"
+    );
 
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
-    listElement.append(postEl);
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
