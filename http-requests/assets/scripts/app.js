a1acda659b7dc6807f11de1a12b09e4dd6e99845
Silver <silverdev@protonmail.com>
2023-10-28 19:14:49 +0300

    Refactor the http request project using axios.

--- http-requests/assets/scripts/app.js
+++ http-requests/assets/scripts/app.js
@@ -1,75 +1,76 @@
-const listElement = document.querySelector(".posts");
-const postTemplate = document.getElementById("single-post");
-const form = document.querySelector("#new-post form");
-const fetchButton = document.querySelector("#available-posts button");
-const postList = document.querySelector("ul");
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+const form = document.querySelector('#new-post form');
+const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
 function sendHttpRequest(method, url, data) {
   // const promise = new Promise((resolve, reject) => {
-  //   const xhr = new XMLHttpRequest();
+  // const xhr = new XMLHttpRequest();
+  // xhr.setRequestHeader('Content-Type', 'application/json');
 
   //   xhr.open(method, url);
 
-  //   xhr.responseType = "json"; // automatically parses to JS; same as using JSON.parse
+  //   xhr.responseType = 'json';
 
-  //   xhr.onload = function () {
+  //   xhr.onload = function() {
   //     if (xhr.status >= 200 && xhr.status < 300) {
   //       resolve(xhr.response);
-  //       //   const listOfPosts = JSON.parse(xhr.response);
   //     } else {
-  //       reject(new Error("Something went wrong!"));
+  // xhr.response;
+  //       reject(new Error('Something went wrong!'));
   //     }
+  //     // const listOfPosts = JSON.parse(xhr.response);
   //   };
 
-  //   xhr.onerror = function () {
-  //     reject(new Error("Failed to send request!"));
+  //   xhr.onerror = function() {
+  //     reject(new Error('Failed to send request!'));
   //   };
 
   //   xhr.send(JSON.stringify(data));
-
   // });
+
   // return promise;
   return fetch(url, {
     method: method,
-    body: data,
     // body: JSON.stringify(data),
+    body: data
     // headers: {
-    //   "Content-type": "application/json",
-    // },
+    //   'Content-Type': 'application/json'
+    // }
   })
-    .then((response) => {
+    .then(response => {
       if (response.status >= 200 && response.status < 300) {
-        return response.json(); // This will parse the body of the response and transform it from json to javascript.
+        return response.json();
       } else {
-        return response.json().then((errData) => {
+        return response.json().then(errData => {
           console.log(errData);
-          throw new Error("Something went wrong server-side");
+          throw new Error('Something went wrong - server-side.');
         });
       }
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error);
-      throw new error("Something went wrong");
+      throw new Error('Something went wrong!');
     });
 }
 
 async function fetchPosts() {
   try {
-    const responseData = await sendHttpRequest(
-      "GET",
-      "https://jsonplaceholder.typicode.com/posts"
+    const response = await axios.get(
+      'https://jsonplaceholder.typicode.com/posts'
     );
-
-    const listOfPosts = responseData;
+    const listOfPosts = response.data;
     for (const post of listOfPosts) {
       const postEl = document.importNode(postTemplate.content, true);
-      postEl.querySelector("h2").textContent = post.title.toUpperCase();
-      postEl.querySelector("p").textContent = post.body;
-      postEl.querySelector("li").id = post.id;
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
       listElement.append(postEl);
     }
   } catch (error) {
     alert(error.message);
+    console.log(error.response);
   }
 }
 
@@ -78,36 +79,33 @@ async function createPost(title, content) {
   const post = {
     title: title,
     body: content,
-    userId: userId,
+    userId: userId
   };
 
   const fd = new FormData(form);
-  // fd.append("title", title);
-  // fd.append("body", content);
-  fd.append("userID", userId);
+  // fd.append('title', title);
+  // fd.append('body', content);
+  fd.append('userId', userId);
 
-  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", fd);
+  const response = await axios.post(
+    'https://jsonplaceholder.typicode.com/posts',
+    fd
+  );
+  console.log(response);
 }
 
-fetchButton.addEventListener("click", () => {
-  fetchPosts();
-});
-form.addEventListener("submit", (event) => {
+fetchButton.addEventListener('click', fetchPosts);
+form.addEventListener('submit', event => {
   event.preventDefault();
-  // Can validate user input here
-  const enteredTitle = event.currentTarget.querySelector("#title").value;
-  const enteredContent = event.currentTarget.querySelector("#content").value;
+  const enteredTitle = event.currentTarget.querySelector('#title').value;
+  const enteredContent = event.currentTarget.querySelector('#content').value;
 
   createPost(enteredTitle, enteredContent);
 });
 
-postList.addEventListener("click", (event) => {
-  if (event.target.tagName == "BUTTON") {
-    // if true, that means the user clicked on a button; we don't want this going off every time the user clicks anywhere
-    const postId = event.target.closest("li").id;
-    sendHttpRequest(
-      "DELETE",
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
-    );
+postList.addEventListener('click', event => {
+  if (event.target.tagName === 'BUTTON') {
+    const postId = event.target.closest('li').id;
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   }
 });
--- javascript-libraries/app.js
+++ javascript-libraries/app.js
@@ -1,5 +1,5 @@
 const customers = ["Max", "Manuel", "Anna"];
 const activeCustomers = ["Max", "Manuel"];
-const inactiveCustomers = _.difference([2, 1], [2, 3]);
+const inactiveCustomers = _.difference(customers, activeCustomers);
 
 console.log(inactiveCustomers);
