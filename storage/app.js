89a7000d71b9a9dce1838fde920779a2edeb0c98
Silver <silverdev@protonmail.com>
2023-11-02 13:20:13 +0200

    Use cookies to send dummy data.

--- storage/app.js
+++ storage/app.js
@@ -1,24 +1,12 @@
+// console.log(document.cookie);
+
 const storeBtn = document.getElementById("store-btn");
 const retrBtn = document.getElementById("retrieve-btn");
 
-const userId = "u123";
-const user = {
-  name: "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-};
-
 storeBtn.addEventListener("click", () => {
-  sessionStorage.setItem("uid", userId);
-  localStorage.setItem("user", JSON.stringify(user));
+  const userId = "u123";
+  document.cookie = `uid=${userId}`;
 });
 retrBtn.addEventListener("click", () => {
-  const extractedId = sessionStorage.getItem("uid");
-  const extractedUser = JSON.parse(localStorage.getItem("user"));
-  console.log(extractedUser);
-  if (extractedId) {
-    console.log("Got the ID - " + extractedId);
-  } else {
-    console.log("Could not find ID");
-  }
+  console.log(document.cookie);
 });
