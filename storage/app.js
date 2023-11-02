586512705b0451803a4c1e500cbcc543c8e5a76c
Silver <silverdev@protonmail.com>
2023-11-02 15:24:12 +0200

    Add more cookie exercises.

--- storage/app.js
+++ storage/app.js
@@ -5,8 +5,14 @@ const retrBtn = document.getElementById("retrieve-btn");
 
 storeBtn.addEventListener("click", () => {
   const userId = "u123";
-  document.cookie = `uid=${userId}`;
+  const user = { name: "Max", age: 30 };
+  document.cookie = `uid=${userId}; max-age=360`;
+  document.cookie = `user=${JSON.stringify(user)}`;
 });
 retrBtn.addEventListener("click", () => {
-  console.log(document.cookie);
+  const cookieData = document.cookie.split(";"); // This is an array.
+  const data = cookieData.map((i) => {
+    return i.trim(); // This is just to remove the excess white space.
+  });
+  console.log(data[1].split("=")[1]); // user value
 });
